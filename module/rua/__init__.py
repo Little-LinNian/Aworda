c5045a969a8e5dbff24fb846bdd7046680f23ec4
Little-LinNian <2544704967@qq.com>
2022-02-17 20:06:52 +0800

    :art: 重新格式化代码

--- module/rua/__init__.py
+++ module/rua/__init__.py
@@ -15,37 +15,31 @@ from graia.ariadne.message.element import Image, Plain, At
 from graia.ariadne.message.chain import MessageChain
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.model import Group, Member
+
 saya = Saya.current()
 channel = Channel.current()
 
 
+data_dir = os.path.join(path.dirname(__file__), "rua_data/data")
 
-data_dir = os.path.join(path.dirname(__file__), 'rua_data/data')
+
 async def ruaer(id):
-    url = f'http://q1.qlogo.cn/g?b=qq&nk={id}&s=160'
-    async with aiohttp.request("get",url) as resp:
+    url = f"http://q1.qlogo.cn/g?b=qq&nk={id}&s=160"
+    async with aiohttp.request("get", url) as resp:
         resp_cont = await resp.read()
     avatar = im.open(BytesIO(resp_cont))
-    output = await asyncio.to_thread(generate_gif,data_dir,avatar)
+    output = await asyncio.to_thread(generate_gif, data_dir, avatar)
     return output
 
 
-@channel.use(ListenerSchema(
-    listening_events=[GroupMessage]
-))
-async def rua(app: Ariadne,member: Member
-        ,group: Group,msg: MessageChain):
-    if msg.asDisplay().startswith('rua') or msg.asDisplay().endswith("rua"):
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def rua(app: Ariadne, member: Member, group: Group, msg: MessageChain):
+    if msg.asDisplay().startswith("rua") or msg.asDisplay().endswith("rua"):
 
-            qid = msg.get(At)[0].target
-            data = await ruaer(qid)
-            ph = Image(data_bytes=data)
-            await app.sendGroupMessage(group,
-                    MessageChain.create(
-                        [ph])
-                    )
+        qid = msg.get(At)[0].target
+        data = await ruaer(qid)
+        ph = Image(data_bytes=data)
+        await app.sendGroupMessage(group, MessageChain.create([ph]))
 
     else:
         pass
-
-
